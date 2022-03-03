a238e02dbf3dd5dd0a92675229d1570d74fcb569
arthurwinck <apwinck@gmail.com>
2022-03-03 12:10:40 -0300

    após segunda entrega

--- ordem_topologica.py
+++ ordem_topologica.py
@@ -2,9 +2,9 @@ from grafo import Grafo, Vertice
 
 def ordenacao_topologica(grafo):
     ordem = []
-    c = [False] * (grafo.qtdVertices()+1)
+    c = [False] * (grafo.qtdVertices())
 
-    for i in range(grafo.qtdVertices()+1):
+    for i in range(grafo.qtdVertices()):
         if c[i] == False:
             dfs_visit_ot(grafo,i,c,ordem)
     
@@ -13,7 +13,8 @@ def ordenacao_topologica(grafo):
     ordem = [int(x-1) for x in ordem]
 
     for k in ordem:
-        new_ordem.append(grafo.rotulo(k))
+        vertice = grafo.vertices[k]
+        new_ordem.append(grafo.rotulo(vertice))
     
     for x in range(len(new_ordem)):
         new_ordem[x] = (' '.join(new_ordem[x]))
@@ -24,8 +25,8 @@ def dfs_visit_ot(grafo,i,c,ordem):
     c[i] = True
     vizinhos = grafo.vizinhos(i+1)
     for j in vizinhos:
-        if c[j] == False:
-            c, ordem = dfs_visit_ot(grafo,j,c,ordem)
+        if c[j-1] == False:
+            c, ordem = dfs_visit_ot(grafo,j-1,c,ordem)
     
     ordem.insert(0,i)
     return (c,ordem)
